5a9970d2b1ec5e2a57a01c019c6028812e08e0fa
IEVN1001-21000579 <102932383+IEVN1001-21000579@users.noreply.github.com>
2024-09-14 07:07:58 -1000

    Commit de intro-ts sábado 13

--- src/temas-ts/10-volumenes.ts
+++ src/temas-ts/10-volumenes.ts
@@ -1,20 +1,4 @@
-class Areas{
-    protected lado1 : number;
-    protected lado2 : number;
-    protected radio : number;
-
-    constructor(lado1:number, lado2:number, radio:number){
-        this.lado1 = lado1;
-        this.lado2 = lado2;
-        this.radio = radio;
-    }
-
-    imprimir(){
-        console.log(`Area de la base del prisma: ${this.lado1 * this.lado2}`);
-        console.log(`Area de la base de la piramide: ${this.lado1 * this.lado2}`);
-        console.log(`Area de la base del cilindro: ${(3.141516) * ((this.radio) ** 2)}`);
-    }
-}
+import { Areas } from "./09-areas.ts";
 
 class Volumenes extends Areas{
     private altura : number;
